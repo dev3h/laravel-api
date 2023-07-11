95a63c3074bb0208cd689144bf307eb8643f8035
nam <namsupermen06@gmail.com>
2023-07-11 23:40:36 +0700

    create api update, delete, get by id contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function index()
+    public function getAllContacts()
     {
         $contacts = Contact::all();
         return response()->json([
@@ -14,5 +15,48 @@ class ContactController extends Controller
             'contacts' => $contacts,
             'message' => 'Contacts retrieved successfully.',
         ]);
+    }
+    public function getContact($id)
+    {
+        $contact = Contact::find($id);
+
+        return response()->json([
+            'status' => 200,
+            'contact' => $contact,
+        ]);
+    }
+    public function storeContact(Request $request)
+    {
+        Contact::create($request->all());
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Contact created successfully.',
+        ]);
+    }
+    public function updateContact($id, Request $request)
+    {
+        $contact = Contact::find($id);
+        $contact->update($request->all());
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Contact updated successfully.',
+        ]);
+    }
+    public function deleteContact($id)
+    {
+        $contact = Contact::find($id);
+        if ($contact) {
+            $contact->delete();
+            return response()->json([
+                'status' => 200,
+                'message' => 'Contact deleted successfully.',
+            ]);
+        } else {
+            return response()->json([
+                'message' => "Contact with id:$id doesn't exists",
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/contacts', [ContactController::class, 'index']);
+Route::get('/contacts', [ContactController::class, 'getAllContacts']);
+Route::get('/contacts/{id}', [ContactController::class, 'getContact']);
+Route::post('/contacts', [ContactController::class, 'storeContact']);
+Route::put('/contacts/{id}', [ContactController::class, 'updateContact']);
+Route::delete('/contacts/{id}', [ContactController::class, 'deleteContact']);
